fb6844c69c3889b56d797c29372d31ce18ddedca
최수빈 <soobin1457@daum.net>
2025-08-26 14:50:27 +0900

    CRUD 연습

--- src/main/java/com/example/learn/spring/LearnSpringApplication.java
+++ src/main/java/com/example/learn/spring/LearnSpringApplication.java
@@ -1,7 +1,22 @@
 package com.example.learn.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearnSpringApplication {
@@ -11,3 +26,109 @@ public class LearnSpringApplication {
 	}
 
 }
+
+class Coffee {
+	private String id;
+	private String name;
+	
+    public Coffee() {
+    	this.id = null;
+    	this.name = null;
+    }
+	
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+    public void setId(String id) {
+        this.id = id;
+    }
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+@RestController
+@RequestMapping("/coffees")
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+	
+	public RestApiDemoController() {
+		coffees.addAll(List.of(
+				new Coffee("Cafe Cereza"),
+				new Coffee("Cafe Ganador"),
+				new Coffee("Cafe Lareno"),
+				new Coffee("Cafe Tres Pontas")
+			));
+	}
+	
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+	
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+				return Optional.of(c);
+			}
+		}
+		
+		return Optional.empty();
+	}
+	
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		coffees.add(coffee);
+		return coffee;
+	}
+	
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+		int coffeeIndex = -1;
+		
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+			coffeeIndex = coffees.indexOf(c);
+			coffees.set(coffeeIndex, coffee);
+			}
+		}
+		
+		return (coffeeIndex == -1) ? 
+				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) : 
+				new ResponseEntity<>(coffee, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id) {
+		coffees.removeIf(c -> c.getId().equals(id));
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
