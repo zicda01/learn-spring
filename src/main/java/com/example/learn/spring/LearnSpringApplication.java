3a8466fc924b08377315e27ab8ffc845001282c0
최수빈 <soobin1457@daum.net>
2025-08-27 17:40:33 +0900

    configuration 어노테이션을 이용해 외부 설정 변수 접근하기

--- src/main/java/com/example/learn/spring/LearnSpringApplication.java
+++ src/main/java/com/example/learn/spring/LearnSpringApplication.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,10 +28,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class LearnSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringApplication.class, args);
+	}
+	
+	@Bean
+	@ConfigurationProperties(prefix = "droid")
+	Droid createDroid() {
+		return new Droid();
 	}
 
 }
@@ -127,6 +138,88 @@ class RestApiDemoController {
 	
 }
 
+@ConfigurationProperties(prefix = "greeting")
+class Greeting {
+	private String name;
+	private String coffee;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getCoffee() {
+		return coffee;
+	}
+	
+	public void setCoffee(String coffee) {
+		this.coffee = coffee;
+	}
+}
+
+@RestController
+@RequestMapping("/greeting")
+class GreetingController {
+	private final Greeting greeting;
+	
+	public GreetingController(Greeting greeting) {
+		this.greeting = greeting;
+	}	
+	
+//	@Value("${greeting-name: Mirage}")
+//	private String name;
+//	
+//	@Value("${greeting-coffee: ${greeting-name: 수빈} is drinking Cafe Ganador}")
+//	private String coffee;
+	
+	@GetMapping
+	String getGreeting() {
+		return greeting.getName();
+	}
+	
+	@GetMapping("/coffee")
+	String getNameAndCoffee() {
+		return greeting.getCoffee();
+	}
+}
+
+class Droid {
+	private String id, description;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
+
+@RestController
+@RequestMapping("/droid")
+class DroidController {
+	private final Droid droid;
+	
+	public DroidController(Droid droid) {
+		this.droid = droid;
+	}
+	
+	@GetMapping
+	Droid getDroid() {
+		return droid;
+	}
+}
 
 
 
